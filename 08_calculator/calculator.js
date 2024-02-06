e1685c71e9387b269f3d987500f69a4ff1921721
doogin306 <codymkeener@gmail.com>
2024-02-05 20:00:29 -0600

    create functions for calculator app

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let addAnswer = num1 + num2;
+  return(addAnswer);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let subtractAnswer = num1 - num2;
+  return(subtractAnswer);
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	let sumArray = input.concat();
+  let sumAnswer = 0;
+
+  if (sumArray.length == 0) {
+    return(0);
+  } else {
+    for (let i = 0; i < sumArray.length; i++) {
+      sumAnswer += sumArray[i];
+    }
+  }
+  return(sumAnswer);
 };
 
-const multiply = function() {
+const multiply = function(input) {
+
+  let multiplyArray = input.concat();
+  let multiplyAnswer = 1;
+
+  if (multiplyArray.length == 0) {
+    return(0);
+  } else {
+    for (let i = 0; i < multiplyArray.length; i++) {
+      multiplyAnswer *= multiplyArray[i];
+    }
+  }
+  return(multiplyAnswer);
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let powerAnswer = num1 ** num2;
+  return(powerAnswer);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let x = input;
+  factorialAnswer = x;
+
+  if (x == 0 || x == 1) {
+    return(1);
+  } else {
+    while (x > 1) {
+      x--;
+      factorialAnswer *= x;
+    }
+  }
+  return(factorialAnswer);
 };
 
 // Do not edit below this line
