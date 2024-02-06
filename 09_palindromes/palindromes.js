a7231a6e8355577e18c20dce9a864afc0d7944a9
doogin306 <codymkeener@gmail.com>
2024-02-05 20:55:23 -0600

    create function to test if string is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let lowerInput = input.toLowerCase();
+    let normalizedInput = lowerInput.replace(/[^a-z0-9]/g, "");
+    let splitInput = normalizedInput.split("");
+    let reverseInput = splitInput.reverse();
+    let joinInput = reverseInput.join("");
+    
+    return joinInput == normalizedInput;
 };
 
 // Do not edit below this line
