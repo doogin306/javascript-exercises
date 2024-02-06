9b3319c8d9fe0dd6effb76ddfc0a3a4b280ab1f9
doogin306 <codymkeener@gmail.com>
2024-02-06 17:30:45 -0600

    create formula to return specific fibonacci value

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+let inputNum = input;
+let fibArray = [0, 1];
+var fibNum;
+
+// for (let i = 1; i < inputNum; inputNum - 1) {
+//     let fibNum = (inputNum - 1) + (inputNum -2);
+//     fibArray.splice(2, 0, fibNum);
+// }
+
+if (inputNum < 0) {
+    return("OOPS");
+} else {
+
+    for (let i = 1; i < inputNum; i++) {
+        let fibNum1 = fibArray[((i + 1) - 1)];
+        let fibNum2 = fibArray[((i + 1) - 2)];
+        fibNum = fibNum1 + fibNum2;
+        fibArray.push(fibNum);
+    }
+}
+return(fibArray[input]);
 
 };
 
