8fa5f10870305612e33994d18d4ce2a8ef61d1e9
doogin306 <codymkeener@gmail.com>
2024-02-09 14:33:04 -0500

    create function to return the titles of the books in the array of books and authors

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(input) {
+    let inputArray = input.concat();
+    let bookArray = inputArray.map(book => book.title);
 
+    // for (i = 0; i < inputArray.length; i++) {
+    //     bookArray.push(inputArray[i]);
+    // }
+
+    return(bookArray.concat());
 };
 
 // Do not edit below this line
