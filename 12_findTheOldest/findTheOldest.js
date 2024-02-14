e38c05aa807cdd1d2cd16bc73fd5be10ca697e19
doogin306 <codymkeener@gmail.com>
2024-02-14 12:08:33 -0600

    create a function to find the age of a person in the array, create a function to compare the ages of each person in the array and return the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
+
+let oldest = input.reduce((oldestPerson, currentPerson) => {
+
+    let ageOldest = getAge(oldestPerson);
+    let ageCurrent = getAge(currentPerson);
+
+    return (ageCurrent > ageOldest) ? currentPerson : oldestPerson;
+}, input[0]);
+
+return(oldest);
 
 };
+
+let getAge = function(person) {
+
+let currentYear = new Date().getFullYear();
+
+let birthYear = person.yearOfBirth;
+let deathYear = person.yearOfDeath || currentYear;
+
+let age = deathYear - birthYear;
+
+return(age);
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
